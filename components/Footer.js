637e62d4bd62a14c3622960cb024be9925eddec8
HasanTheViking <163563125+HasanTheViking@users.noreply.github.com>
2025-05-26 08:01:17 +0200

    Update Footer.js
    .

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-dark text-light text-center p-4 mt-8">
+    <footer className="text-light text-center p-4 mt-8">
       © {new Date().getFullYear()} AutoDex – Všetky práva vyhradené
     </footer>
   );
