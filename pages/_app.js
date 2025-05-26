f47c9d64b9d804e16473b51deb3009acfb3dbbaa
HasanTheViking <163563125+HasanTheViking@users.noreply.github.com>
2025-05-26 07:51:59 +0200

    Update _app.js
    color gray

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
-import '../styles/globals.css';
-import { CartProvider } from '../components/CartContext';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+// pages/_app.js
+import '../styles/globals.css'
+import { CartProvider } from '../components/CartContext'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <div className="flex flex-col min-h-screen bg-gray-800 text-white">
+        <Header />
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
     </CartProvider>
-  );
+  )
 }
